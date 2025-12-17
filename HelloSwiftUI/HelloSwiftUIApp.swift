50d2013227c0797be8df8fe11670e5c2d617008e
levanthanhlong <thanhtdbn132001@gmail.com>
2025-12-17 10:42:08 +0700

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by ThanhLe on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloSwiftUITests/HelloSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUITests.swift
+//  HelloSwiftUITests
+//
+//  Created by ThanhLe on 17/12/25.
+//
+
+import Testing
+@testable import HelloSwiftUI
+
+struct HelloSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
